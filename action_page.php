ed8f1e226090b29ddf8ebbbbe198d303309da5d8
atilalintz <amllintz@gmail.com>
2023-10-28 09:23:54 -0300

    Formulário.1.0

---
+++ action_page.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemplo de Formulário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php
+$checkbox = $_POST["check"];
+$color = $_POST["color"];
+$date = $_POST["date"];
+$datetime_local = $_POST["datetime-local"];
+$email = $_POST["email"];
+$file = $_POST["file"];
+$hidden = $_POST["hidden"];
+$mes = $_POST["mes"];
+$image = $_POST["image"];
+$numero = $_POST["numero"];
+$senha = $_POST["senha"];
+$sexo = $_POST["sexo"];
+$range = $_POST["range"];
+$reset = $_POST["reset"];
+$procurar = $_POST["procurar"];
+$telefone = $_POST["telefone"];
+$testo = $_POST["testo"];
+$tempo = $_POST["tempo"];
+$url = $_POST["url"];
+$semana = $_POST["semana"];
+$pnome = $_POST["pnome"];
+$unome = $_POST["unome"];
+$testearea = $_POST["testearea"];
+
+
+echo $checkbox;
+echo $color;
+echo $date;
+echo $datetime_local;
+echo $email;
+echo $file;
+echo $hidden;
+echo $mes;
+echo $image;
+echo $numero;
+echo $senha;
+echo $sexo;
+echo $range;
+echo $procurar;
+echo $telefone;
+echo $testo;
+echo $tempo;
+echo $url;
+echo $semana;
+echo $pnome;
+echo $unome;
+echo $time;
+echo $testearea
+?>
+
+</body>
+</html> 
